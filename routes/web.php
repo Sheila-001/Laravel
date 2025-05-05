ff888fd35419de86f766883e715ab3983f11d418
Sheila-001 <sheilabelarmino377@gmail.com>
2025-05-05 22:26:00 +0800

    added register.blade.php

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,9 @@ use App\Http\Controllers\AuthController;
 Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Protected routes
